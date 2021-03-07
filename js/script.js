108ec7d8428c90abe0c27d89d8f3f586b735ff2a
MAHDI <mahdigholizadek@gmail.com>
2021-03-07 16:36:27 +0330

    all in vue.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,20 @@
-$(document).ready(function(){
-    $(".inp").on("input",function(e){
-        var length = $(this).val().length ;
-        if(length < 8 ){
-            $(this).css("background","red") ;
-        }else if(length < 12){
-            $(this).css("background","rgb(198, 209, 44)") ;
-        }else{
-            $(this).css("background","green") ;
-        }
-    })
-});
-
-
 var showText = new Vue({
     el:".input-box",
     data:{
         title : "",
     }, methods: {
         showSentence : function(e){
-            if(e.target.value.length < 8){
-                this.title = "password is too weak !"
-            }else if(e.target.value.length < 12){
-                this.title = "password is not strong enogh !"
+            var input = e.target ;
+            var length = e.target.value.length ;
+            if(length < 8){
+                this.title = "password is too weak !" ;
+                input.style.background = "red" ;
+            }else if(length < 12){
+                this.title = "password is not strong enogh !" ;
+                input.style.background = "yellow" ;
             }else{
                 this.title = "password is safe and strong !"
+                input.style.background = "green" ;
             }
         }
     },
