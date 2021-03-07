700c052223468ebb66db85d4ede81d0075d19054
MAHDI <mahdigholizadek@gmail.com>
2021-03-07 13:19:54 +0330

    firstPassChecker

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $(".inp").on("input",function(){
+        var length = $(this).val().length ;
+        if(length < 8 ){
+            $(this).css("background","red") ;
+        }else if(length < 12){
+            $(this).css("background","rgb(198, 209, 44)") ;
+        }else{
+            $(this).css("background","green") ;
+        }
+    })
+
+
+});
